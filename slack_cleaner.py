cdd460d22dc17c856a0f20f7581a2ec2c1bcaafa
Jan Nylund <jan.nylund@mac.com>
2019-04-06 22:25:47 +0300

    Added a feature to list non-archived private channels. (#3)

--- slack_cleaner.py
+++ slack_cleaner.py
@@ -6,7 +6,7 @@ import os
 TOKEN = os.getenv('SLACKTOKEN')
 USERS_LIST_URL = "https://slack.com/api/users.list?token=" + TOKEN
 ACCESS_LOGS_URL = "https://slack.com/api/team.accessLogs?token=" + TOKEN + "&count=1000"
-CONVERSATIONS_LIST_URL = "https://slack.com/api/conversations.list?token=" + TOKEN + "&limit=1000"
+CONVERSATIONS_LIST_URL = "https://slack.com/api/conversations.list?token=" + TOKEN + "&limit=1000&exclude_archived=true"
 CONVERSATIONS_MEMBERS_URL = "https://slack.com/api/conversations.members?token=" + TOKEN
 SLACK_BOT_ID = "USLACKBOT"
 PERIOD = 90
@@ -57,11 +57,15 @@ def get_users_list():
     return users
 
 
-def get_conversations_list():
+def get_conversations_list(private = False):
     conversations = {}
+    if private:
+        channel_types = "private_channel"
+    else:
+        channel_types = "public_channel"
 
-    print("Fetching all conversations...", end="", flush=True)
-    response = request.urlopen(CONVERSATIONS_LIST_URL)
+    print("Fetching {} conversations...".format(channel_types), end="", flush=True)
+    response = request.urlopen(CONVERSATIONS_LIST_URL + "&types=" + channel_types)
     response_json = json.loads(response.read().decode(response.info().get_content_charset()))
     if response_json["ok"] is False:
         raise Exception("Slack API returned error: " + response_json["error"])
@@ -128,27 +132,45 @@ def filter_users(all_users, user_type):
 
 
 def print_inactive_users(all_users, active_users, user_type):
+    print_separator()
     users = filter_users(all_users, user_type)
-    print("------------------------------------------")
     print("These " + user_type + " users have not logged in during the last " + str(PERIOD) + " days:")
     inactive_users = set(users.keys()) - set(active_users.keys())
     for user in inactive_users:
         print(" - " + all_users[user]["profile"]["email"])
+    print_separator()
 
 
 def print_single_channel_licensed_users(all_users, users_and_channels):
-    print("------------------------------------------")
+    print_separator()
     print("These licensed users are members of less than 2 channels:")
     for user, channels in users_and_channels.items():
         if len(channels) < 2 and user in all_users and is_licensed(all_users[user]):
             print(" - " + all_users[user]["profile"]["email"] + "(" + user + ") belongs to #" + ", #".join(channels))
+    print_separator()
+
+
+def print_private_channels():
+    print_separator()
+    print("Listing all (non-archived) private channels:")
+    channels = get_conversations_list(private = True)
+    for chan in channels:
+        print("- #" + channels[chan]["name"])
+    print_separator()
+
+
+def print_separator():
+    print("------------------------------------------")
 
 
 if __name__ == "__main__":
     all_users = get_users_list()
-    all_channels = get_conversations_list()
+    all_channels = get_conversations_list(private = False)
     users_and_channels = get_users_and_channels(all_channels)
     active_users = get_lately_logged_in_users()
     print_inactive_users(all_users, active_users, "licensed")
     print_inactive_users(all_users, active_users, "free")
     print_single_channel_licensed_users(all_users, users_and_channels)
+
+    # Find private channels and list them.
+    print_private_channels()
